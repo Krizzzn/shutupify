650bbd3105e268716016dd459ae9b6863fe64045
Christian Peterek <christian.peterek@gmail.com>
2013-04-20 17:48:18 +0200

    [ADD] VLC Controller

---
+++ Shutupify/Jukeboxes/Drivers/Vlcmote.cs
@@ -0,0 +1,176 @@
+﻿
+//// INSPIRED BY CODE from Sam Saffron https://github.com/SamSaffron (https://gist.github.com/SamSaffron/101357)
+//// http://www.autoitscript.com/forum/topic/76230-vlc-automation-over-tcp/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace Shutupify.Jukeboxes.Drivers {
+
+    public class Vlcmote : IDisposable {
+        DateTime _lastPlayingQuery;
+        bool _lastAnswer;
+        bool _clientCantBeFound;
+        private TcpClient _client;
+
+        public int Port { get { return 9972; } }
+
+        public void Play()
+        {
+            if (IsPlaying())
+                return;
+            Toggle();
+        }
+
+        public void Pause()
+        {
+            if (!IsPlaying())
+                return;
+            Toggle();
+        }
+
+        public void Toggle() {
+            SendCommand("pause", null);
+            _lastPlayingQuery = DateTime.MinValue;
+        }
+
+        public void Next()
+        {
+            SendCommand("next", null);
+            _lastPlayingQuery = DateTime.MinValue;
+        }
+
+        public void Previous()
+        {
+            SendCommand("prev", null);
+            _lastPlayingQuery = DateTime.MinValue;
+        }
+
+        public bool IsPlaying() {
+            if (DateTime.Now.Subtract(_lastPlayingQuery).TotalSeconds < 2)
+                return _lastAnswer;
+            var message = SendCommandAndReadResult("status", null);
+            _lastPlayingQuery = DateTime.Now;
+            _lastAnswer =  message.Contains("play state: 3");
+            return _lastAnswer;
+        }
+
+        private TcpClient Client {
+            get {
+                if (_client != null && !_client.Connected) {
+                    _client.Close();
+                    _client = null;
+                }
+
+                if (_client == null)
+                {
+                    if (_clientCantBeFound)
+                        return null;
+
+                    try
+                    {
+                        _client = new TcpClient("localhost", this.Port);
+                    }
+                    catch (System.Net.Sockets.SocketException)
+                    {
+                        _clientCantBeFound = true;
+                    }
+                }
+                return _client;
+            }
+        }
+
+        public string SendCommandAndReadResult(string command, string param)
+        {
+            SendCommand(command, param);
+            Thread.Sleep(100);
+            return ReadTillEnd();
+        }
+
+        void SendCommand(string command, string param)
+        {
+            if (Client == null)
+                return;
+
+            ReadTillEnd();
+
+            string packet = command;
+            if (param != null)
+                packet += " " + param;
+
+            packet += Environment.NewLine;
+            
+            var buffer = ASCIIEncoding.ASCII.GetBytes(packet);
+            try
+            {
+                Client.GetStream().Write(buffer, 0, buffer.Length);
+                Client.GetStream().Flush();
+            }
+            catch (System.IO.IOException)
+            {
+                Client.Close();
+                _client = null;
+            }
+        }
+
+        private string ReadTillEnd()
+        {
+            if (Client == null)
+                return "";
+
+            StringBuilder sb = new StringBuilder();
+            while (Client.GetStream().DataAvailable)
+            {
+                int b = Client.GetStream().ReadByte();
+                if (b >= 0)
+                {
+                    sb.Append((char)b);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return sb.ToString();
+        }
+
+        #region IDisposable
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (Client != null)
+                    {
+                        SendCommand("logout", "");
+                        Thread.Sleep(100);
+                        Client.Close();
+                        _client = null;
+                    }
+                }
+
+                disposed = true;
+            }
+        }
+
+        public void Dispose() // Implement IDisposable
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Vlcmote() // the finalizer
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
---
+++ Shutupify/Jukeboxes/VLController.cs
@@ -0,0 +1,103 @@
+﻿using Shutupify.Jukeboxes.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Shutupify.Jukeboxes
+{
+    public class VLController : IJukebox, IDisposable
+    {
+        Vlcmote _remote;
+        Dictionary<JukeboxCommand, string> _actionMapping;
+
+        public VLController()
+        {
+            _remote = new Vlcmote();
+
+            _actionMapping = new Dictionary<JukeboxCommand, string>();
+            _actionMapping.Add(JukeboxCommand.Play, "Play");
+            _actionMapping.Add(JukeboxCommand.Pause, "Pause");
+            _actionMapping.Add(JukeboxCommand.NextTrack, "Next");
+            _actionMapping.Add(JukeboxCommand.PreviousTrack, "Previous");
+            _actionMapping.Add(JukeboxCommand.Toggle, "Toggle");
+            _actionMapping.Add(JukeboxCommand.PlayAfterPaused, "Play");
+        }
+
+        public void PerformAction(JukeboxCommand cmd)
+        {
+            if (_remote == null)
+                return;
+
+            if (!_actionMapping.ContainsKey(cmd))
+                return;
+
+            var methodToCall = _actionMapping[cmd];
+            _remote.GetType().GetMethod(methodToCall).Invoke(_remote, new object[0]);
+        }
+
+        public bool IsActive
+        {
+            get
+            {
+                return true;
+            }
+            set{
+            
+            }
+        }
+
+        public bool IsAvailable
+        {
+            get {
+                var avail = Process.GetProcesses()
+                    .Any(proc => proc.ProcessName.Equals("vlc", StringComparison.InvariantCultureIgnoreCase));
+                if (!avail && _remote != null)
+                    _remote.Dispose();
+                return avail;
+            }
+        }
+
+        public bool IsPlaying
+        {
+            get { return _remote.IsPlaying(); }
+        }
+
+        public string Name
+        {
+            get { return "VLC"; }
+        }
+
+        #region IDisposable
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (_remote != null)
+                    {
+                        _remote.Dispose();
+                        _remote = null;
+                    }
+                }
+                disposed = true;
+            }
+        }
+
+        public void Dispose() // Implement IDisposable
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~VLController() // the finalizer
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
